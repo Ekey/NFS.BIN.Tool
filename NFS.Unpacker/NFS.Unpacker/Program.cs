1dceee44f2d0fd01ee857b9e71e996592414de5b
Ekey <infinirt@hotmail.com>
2021-12-15 20:18:30 +0300

    Main

---
+++ NFS.Unpacker/NFS.Unpacker/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace NFS.Unpacker
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Need for Speed ZDIR Unpacker");
+            Console.WriteLine("(c) 2021 Ekey (h4x0r) / v{0}\n", Utils.iGetApplicationVersion());
+            Console.ResetColor();
+
+            if (args.Length != 2)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("[Usage]");
+                Console.WriteLine("    NFS.Unpacker <m_File> <m_Directory>\n");
+                Console.WriteLine("    m_File - Source of ZDIR.BIN index file");
+                Console.WriteLine("    m_Directory - Destination directory\n");
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("[Examples]");
+                Console.WriteLine("    RS.Unpacker E:\\Games\\NFS\\ZDIR.BIN D:\\Unpacked");
+                Console.ResetColor();
+                return;
+            }
+
+            UInt32 test = BinHash.iGetHash(@"CHARACTERS\MCOP02.BIN");
+
+            String m_BinFile = args[0];
+            String m_Output = Utils.iCheckArgumentsPath(args[1]);
+
+            if (!File.Exists(m_BinFile))
+            {
+                Utils.iSetError("[ERROR]: Input file -> " + m_BinFile + " <- does not exist");
+                return;
+            }
+
+            BinUnpack.iDoIt(m_BinFile, m_Output);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# NFS.Unpacker :see_no_evil:
+Tool for extract ZZDATA archives from Need for Speed console games series
+
+# Tested games
+| Game   | Platform   |
+|---      |---    |
+| Need for Speed: Carbon | PS2 |
+| Need for Speed: Carbon | Wii |
